a7ecc85daf37fb78a114bc908294b095bf323c1d
Shalunoff-m <Shalunoff-m@yandex.ru>
2023-11-25 20:30:44 +0300

    feat: + подключение к mongo-db

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+// ПОДКЛЮЧАЕМСЯ К БАЗЕ ДАННЫХ
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+// MIDDLEWARES ----------------------------------------------
+// ПАРСЕР ДАННЫХ
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send({
     answer: 'GET запрос успешно реализован',
+  });
+});
+
+app.post('/', (req, res) => {
+  const { text } = req.body;
+  const answerText = `${text} вот что ты прислал ублюдок`;
+
+  res.send({
+    'answer-text': answerText,
   });
 });
 
