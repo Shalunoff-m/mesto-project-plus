a8aad5b7e2fd27742426b14e461415a6db36c454
Shalunoff-m <Shalunoff-m@yandex.ru>
2023-11-26 17:32:50 +0300

    feat: изменена схма польз. с верификацией почты и пароля

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,27 +1,52 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 interface IUser{
+  email: string,
+  password: string,
   name: string,
   about: string,
   avatar: string,
 }
 
 const userSchema = new mongoose.Schema<IUser>({
-  name: {
+  email: {
     type: String,
     required: true,
+    unique: true,
+    validate: {
+      validator(email:string) {
+        return validator.isEmail(email);
+      },
+      message: (props) => `${props.value} не является действительным адресом электронной почты!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 200,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(url:string) {
+        return validator.isURL(url);
+      },
+      message: (props) => `${props.value} не является ссылкой на изображение`,
+    },
+
   },
 });
 
