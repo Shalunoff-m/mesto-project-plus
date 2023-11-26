d00a4ab4a536485c65ad6470445c92aba80115dd
Shalunoff-m <Shalunoff-m@yandex.ru>
2023-11-26 11:51:02 +0300

    feat: реализованы частные случаи ошибок в users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
+import CustomError from '../helpers/customError';
 import Users from '../models/users';
 
 export const getUserById = (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
   Users.findById(id).then((user) => {
+    if (!user) {
+      throw new CustomError('Такого пользователя нет', 404);
+    }
+
     res.send(user);
   }).catch((err) => {
     next(err);
@@ -23,17 +28,36 @@ export const createUser = (req: Request, res: Response, next: NextFunction) => {
   Users.create(data).then((user) => {
     res.send(user);
   }).catch((err) => {
+    if (err.name === 'ValidationError') {
+      next(new CustomError(`Ошибка валидации: ${err.message}`, 400));
+    } else {
+      next(err);
+    }
+
     next(err);
   });
 };
 
 export const updateUser = (req:any, res: Response, next: NextFunction) => {
   const { _id } = req.user;
-  const newData = req.body;
-  Users.findByIdAndUpdate(_id, { ...newData }, { new: true, runValidators: true })
+  const { name, about } = req.body;
+  if (!name || !about) {
+    throw new CustomError('Введены не все данные', 400);
+  }
+
+  Users.findByIdAndUpdate(_id, { name, about }, { new: true, runValidators: true })
     .then((updatedUser) => {
+      if (!updatedUser) {
+        throw new CustomError('Пользователь не найден', 404);
+      }
+
       res.send(updatedUser);
     }).catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new CustomError(`Ошибка валидации: ${err.message}`, 400));
+      } else {
+        next(err);
+      }
       next(err);
     });
 };
@@ -42,8 +66,16 @@ export const updateAvatar = (req:any, res: Response, next: NextFunction) => {
   const { _id } = req.user;
   const { avatar } = req.body;
 
+  if (!avatar) {
+    throw new CustomError('Нет данных для обновления', 400);
+  }
+
   Users.findByIdAndUpdate(_id, { avatar }, { new: true })
     .then((updatedAvatar) => {
+      if (!updatedAvatar) {
+        throw new CustomError('Пользователь не найден', 404);
+      }
+
       res.send(updatedAvatar);
     }).catch((err) => {
       next(err);
